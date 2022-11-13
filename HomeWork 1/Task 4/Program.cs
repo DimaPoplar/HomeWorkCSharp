d8567aa9099c4d582b40706863c4a7fc8d66b399
DimaGrishin <amvlastik@gmail.com>
2022-11-14 00:12:40 +0300

    4(8) задача

---
+++ HomeWork 1/Task 4/Program.cs
@@ -0,0 +1,51 @@
+﻿/*
+Задача 8: Напишите программу, которая на вход принимает число (N), 
+а на выходе показывает все чётные числа от 1 до N.
+*/
+
+
+Console.Write("Напишите число, ");
+int num = int.Parse(Console.ReadLine()!);
+int score = 1;
+
+// 1 ВАРИАНТ
+/*
+while (score < num + 1)
+{
+    if (score % 2 == 0)
+    {
+        Console.Write($"{score}, ");
+    }
+    score++;
+}
+*/
+
+// 2 ВАРИАНТ (Есть точка в конце вывода чисел)
+/*
+while (score < num + 1)
+{
+    if (num % 2 == 0)
+    {
+        if (score % 2 == 0)
+        {
+            if (score == num) Console.Write($"{num}.");
+            else
+            {
+                Console.Write($"{score}, ");
+            }
+        }
+    }
+    else
+    {
+        if (score % 2 == 0)
+        {
+            if (score == num - 1) Console.Write($"{num - 1}.");
+            else
+            {
+                Console.Write($"{score}, ");
+            }
+        }
+    }
+score++;
+}
+*/
