9529a225f6154441f6f665dac73b415cb60bb761
DimaGrishin <amvlastik@gmail.com>
2022-11-18 22:22:56 +0300

    5(13) Задача

---
+++ HomeWork 2/Task 2/Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, 
+что третьей цифры нет. Берем числа до 100000
+*/
+
+Console.Write("Введите число: ");
+string num = Console.ReadLine()!;
+if (num.Length >= 3 && num.Length <= 6)
+{
+    int num1 = Convert.ToInt32(num);
+    if (num1 > num1 * -1)
+    {
+        Console.WriteLine(num[2]);
+    }
+    else
+    {
+        Console.WriteLine(num[3]);
+    }
+
+}
+else
+{
+    if (num.Length >= 6)
+    Console.WriteLine("Число слишком большое");
+    else 
+    {
+        Console.WriteLine("3-й цифры нет");
+    }
+}
