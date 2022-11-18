2f3f33ebdee06d9b4247817f34afd821c67a383a
DimaGrishin <amvlastik@gmail.com>
2022-11-18 12:28:04 +0300

    5(10) задача, 2 варианта

---
+++ HomeWork 2/Task 1/Program.cs
@@ -0,0 +1,45 @@
+﻿/*
+Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на 
+выходе показывает вторую цифру этого числа. 
+Обязательна проверка на ввод чисел больше/меньше 3-х знаков
+*/
+// 1 ВАРИАНТ
+
+Console.Write("Введите 3-х значное число, ");
+string num = Console.ReadLine()!;
+int num1 = int.Parse(num);
+int num2 = num1 * -1;
+Console.WriteLine(num1);
+
+if (num.Length == 3)
+{
+    Console.WriteLine($"Второе число - {num[1]}");
+}
+else
+{
+    if (num2 > 99 && num2 < 1000)
+    {
+        Console.WriteLine($"Второе число - {num[2]}");
+    }
+    else
+    {
+        Console.WriteLine("Это не 3-х значное число");
+    }
+}
+
+
+
+// 2 ВАРИАНТ
+/*
+Console.Write("Введите 3-х значное число, ");
+int num = int.Parse(Console.ReadLine());
+if (99 < num && num < 1000 || -1000 < num && num < -99)
+{
+    int two = (num / 10) % 10;
+    Console.WriteLine($"Второе число - {two}");
+}
+else
+{
+    Console.WriteLine("Это не 3-х значное число");
+}
+*/
