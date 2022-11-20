05703a12298aadeb69563a2f20a2f6935008f41d
DimaGrishin <amvlastik@gmail.com>
2022-11-21 00:09:39 +0300

    8(21) задача

---
+++ HomeWork 3/Task 2/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+Задача 21
+Напишите программу, которая принимает на вход координаты 
+двух точек и находит расстояние между ними в 3D пространстве.
+*/
+
+Console.Write("Введите 1 координату точки A - ");
+double Anum = double.Parse(Console.ReadLine()!);
+Console.Write("Введите 2 координату точки A - ");
+double Anum1 = double.Parse(Console.ReadLine()!);
+Console.Write("Введите 3 координату точки A - ");
+double Anum2 = double.Parse(Console.ReadLine()!);
+
+Console.Write("Введите 1 координату точки B - ");
+double Bnum = double.Parse(Console.ReadLine()!);
+Console.Write("Введите 2 координату точки B - ");
+double Bnum1 = double.Parse(Console.ReadLine()!);
+Console.Write("Введите 3 координату точки B - ");
+double Bnum2 = double.Parse(Console.ReadLine()!);
+
+double lon = Math.Sqrt(Math.Pow(Bnum - Anum, 2) + Math.Pow(Bnum1 - Anum1, 2) + Math.Pow(Bnum2 - Anum2, 2));
+Console.WriteLine($"Длинна = {lon:f2}");
