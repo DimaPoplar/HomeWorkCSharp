f1cd226af43c290ade29727340d58113a61da648
DimaGrishin <amvlastik@gmail.com>
2022-11-28 21:27:01 +0300

    12(29) задача

---
+++ HomeWork 4/Task 3/Program.cs
@@ -0,0 +1,15 @@
+﻿/*
+Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран. (числа берете любые)
+*/
+
+Console.WriteLine($"Массив - [{String.Join(", ", (NewMass(8)))}]");
+
+int[] NewMass(int size)
+{
+    int[] mass = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        mass[i] = new Random().Next(1, 100);
+    }
+    return mass;
+}
