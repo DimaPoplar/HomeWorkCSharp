1317779cea6268559c03b83bbcd4773a30f94ecb
DimaGrishin <amvlastik@gmail.com>
2022-11-28 20:00:38 +0300

    10(25) задача

---
+++ HomeWork 4/Task 1/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B. 
+(Задачи, решенные через Math.Pow, будут отправлены на переделку, так как задача стоит в том, чтобы написать цикл)
+*/
+
+Console.Write("Введите число А: ");
+int A = int.Parse(Console.ReadLine()!);
+Console.Write("Введите число B: ");
+int B = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine($"Число A в натуральной степене B = {mult(A, B)}");
+
+int mult(int num1, int num2)
+{
+    int result = 1;
+    for (int i = 0; i < num2; i++)
+    {
+        result *= num1;
+    }
+    return result;
+}
