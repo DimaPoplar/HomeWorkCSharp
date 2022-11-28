4a1c446692d9e6d430a79ce4513e9fac375556c8
DimaGrishin <amvlastik@gmail.com>
2022-11-28 23:15:15 +0300

    16(38) задача

---
+++ HomeWork 5/Task 4/Program.cs
@@ -0,0 +1,34 @@
+﻿/*
+Задача 38: Задайте массив целых чисел от -10 до 10. 
+Найдите разницу между максимальным и минимальным элементов массива.
+*/
+
+Console.Write("Введите размер массива: ");
+int A = int.Parse(Console.ReadLine()!);
+
+int[] mass1 = NewMass(A);
+Console.WriteLine($"[{String.Join(", ", (mass1))}]");
+Console.WriteLine($"Разница между min и max элементами = {MinMax(mass1)}");
+
+int[] NewMass(int size)
+{
+    int[] mass = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        mass[i] = new Random().Next(-10, 10);
+    }
+    return mass;
+}
+
+int MinMax(int[] mass)
+{
+    int max = 0;
+    int min = 0;
+    for (int i = 0; i < mass.Length; i++)
+    {
+        if (mass[i] > max) max = mass[i];
+        if (mass[i] < min) min = mass[i];
+    }
+    int result = max - min;
+    return result;
+}
