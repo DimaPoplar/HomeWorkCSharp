cd10794a6138ac7e80e427c7148fb2eb9dcc17fb
DimaGrishin <amvlastik@gmail.com>
2022-11-28 21:53:16 +0300

    13(34) задача

---
+++ HomeWork 5/Task 1/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами.
+Напишите программу, которая покажет количество чётных чисел в массиве.
+*/
+Console.Write("Введите размер массива: ");
+int A = int.Parse(Console.ReadLine()!);
+int[] mass1 = NewMass(A);
+Console.WriteLine($"[{String.Join(", ", (mass1))}]");
+Console.WriteLine($"Кол-во чётных чисел = {chet(mass1)}");
+int[] NewMass(int size)
+{
+    int[] mass = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        mass[i] = new Random().Next(100, 1000);
+    }
+    return mass;
+}
+
+int chet(int[] mass)
+{
+    int count = 0;
+    for (int i = 0; i < mass.Length; i++)
+    {
+        if (mass[i] % 2 == 0)
+        {
+            count = count + 1;
+        }
+    }
+    return count;
+}
